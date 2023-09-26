8b8df14746da1334d7fa0b42f49f5458c9de7514
EvanGruhlkey <gruhlkeyevan@gmail.com>
2023-09-25 23:10:56 -0500

    More Backtesting

--- Stock_predictor.py
+++ Stock_predictor.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import precision_score
 from sklearn.ensemble import RandomForestClassifier # randomize parameters
-TSLA = yf.Ticker("TSLA") #only works for bigger stocks
+TSLA = yf.Ticker("^GSPC") 
 TSLA = TSLA.history(period = "max")
 TSLA.index
 TSLA.plot.line(y = "Open", use_index = True)
@@ -15,8 +15,10 @@ TSLA["Tomorrow"] = TSLA["Close"].shift(-1)
 TSLA["Target"] = (TSLA["Tomorrow"] > TSLA["Close"]).astype(int)
 TSLA = TSLA.loc["1990-01-01":].copy()
 model = RandomForestClassifier(n_estimators = 100, min_samples_split = 100, random_state = 1)
+
 train = TSLA.iloc[:-100]
 test = TSLA.iloc[-100:]
+
 predictors = ["Close", "Volume", "Open", "High", "Low"]
 model.fit(train[predictors], train["Target"])
 preds = model.predict(test[predictors])
@@ -32,7 +34,7 @@ def predict(train, test, predictors, model):
      combined = pd.concat([test["Target"], preds], axis = 1)
      return combined
 
-def backtest(data, model, predictors, start = 2500, step = 250):
+def backtest(data, model, predictors, start = 4000, step = 1):
         all_predictions = []
 
         for i in range(start, data.shape[0], step):
@@ -43,9 +45,12 @@ def backtest(data, model, predictors, start = 2500, step = 250):
         return pd.concat(all_predictions)
 
 predictions = backtest(TSLA, model, predictors)
+
 #print(predictions["Predictions"].value_counts())
 #print(precision_score(predictions["Target"], predictions["Predictions"]))
+
 horizons = [2, 5, 60, 250, 1000]
+
 new_predictors = []
 for horizon in horizons:
         rolling_averages = TSLA.rolling(horizon).mean()
@@ -54,7 +59,9 @@ for horizon in horizons:
         trend_column = f'Trend_{horizon}'
         TSLA[trend_column] = TSLA.shift(1).rolling(horizon).sum()["Target"]
         new_predictors += [ratio_column, trend_column]
+
 print(TSLA)
+
 TSLA = TSLA.dropna()
 model = RandomForestClassifier(n_estimators = 200, min_samples_split = 50, random_state = 1)
 def predict(train, test, predictors, model):
@@ -65,8 +72,13 @@ def predict(train, test, predictors, model):
      preds = pd.Series(preds, index = test.index, name = "Predictions")
      combined = pd.concat([test["Target"], preds], axis = 1)
      return combined
+
 print(predictions["Predictions"].value_counts())
-#predictions = backtest(TSLA, model, new_predictors)
+
+predictions = backtest(TSLA, model, new_predictors)
+
 print(precision_score(predictions["Target"], predictions["Predictions"]))
+
 TSLA.plot.line(y = "Trend_1000", use_index = True)
+
 plt.show()
